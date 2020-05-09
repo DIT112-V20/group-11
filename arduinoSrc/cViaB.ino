343c2f69c35e223b90449387cede4b4bfda7f42d
chanelisilva <chaneli@student.chalmers.se>
2020-05-10 00:13:58 +0200

    Fixed errors in the code

--- arduinoSrc/cViaB.ino
+++ arduinoSrc/cViaB.ino
@@ -23,17 +23,18 @@ void loop() {
 void handleInput() { //handle serial input if there is any
   if (bluetooth.available()) {
     char input;
-    while (bluetooth.available()) { input = bluetooth.read() }; //read till last character
+    while (bluetooth.available()) { 
+      input = bluetooth.read(); } //read till last character
     switch (input) {
-      case'l': //rotate counter-clockwise going forward        
+      case'l': //rotate left going forward        
        car.setSpeed(80); //80% of the full speed        
        car.setAngle(-75); //75 degrees to the left
        break;
-      case'r': //turn clock-wise        
+      case'r': //turn right        
        car.setSpeed(80);        
        car.setAngle(75);  //75 to the right
        break;
-      case'f': //go ahead        
+      case'f': //go forward        
        car.setSpeed(100);        
        car.setAngle(0);
        break;
@@ -41,12 +42,13 @@ void handleInput() { //handle serial input if there is any
        car.setSpeed(-100);        
        car.setAngle(0);
        break;
+      case's': //stop
+       car.setSpeed(0);
+       car.setAngle(0);
+       break;
       default://if you receive something that you don't know, just stop        
        car.setSpeed(0);        
-       car.setAngle(0);    
-      }  
-      }
-      }
+       car.setAngle(0);     
     }
   }
 }
