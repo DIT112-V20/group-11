fcb30344c7ef4663a98426f954e214a1b184947b
AudreyLorna <audreylorna3347@gmail.com>
2020-05-15 15:37:17 +0200

    Added the welcome page

---
+++ TrailGage/app/src/main/java/com/example/trailgage/Control.kt
@@ -0,0 +1,14 @@
+package com.example.trailgage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Control : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_control)
+    }
+
+
+}
