e440b7dcce6883ff6d81d7bf45dbac9532181416
AudreyLorna <audreylorna3347@gmail.com>
2020-05-23 20:29:14 +0200

    Connected the welcome page to the control page, Changed the button radius

--- TrailGage/app/src/main/java/com/example/trailgage/MainActivity.kt
+++ TrailGage/app/src/main/java/com/example/trailgage/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.trailgage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        containedButton.setOnClickListener {
+            val intent = Intent(this, Control::class.java)
+            // start your next activity
+            startActivity(intent)
+        }
     }
+
 }
